0ed7d15df164fdc6f593d808e52e98b11abdfc93
Rituparna sharma <sharmarituparna66@gmail.com>
2024-01-27 16:00:40 +0530

    change db

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,8 +1,12 @@
 
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain.vectorstores import Chroma
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from langchain.vectorstores import Chroma
+import pickle
 
 def load_vectors():
-    gemini_embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    loaded_vector_store = Chroma(persist_directory="models/chroma_db", embedding_function=gemini_embeddings)
+        # To load the vector store later
+    with open('models/modeldb.pkl', 'rb') as f:
+        loaded_vector_store = pickle.load(f)
+    # gemini_embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    # loaded_vector_store = Chroma(persist_directory="models/chroma_db", embedding_function=gemini_embeddings)
     return loaded_vector_store
